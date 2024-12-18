0d5627029421be636742431662b190404a45e359
Aarush-Kanipakam <157634160+Aarush-Kanipakam@users.noreply.github.com>
2024-12-18 23:28:06 +0530

    Create king_activity.h

---
+++ aarushToga/src/king_activity.h
@@ -0,0 +1,9 @@
+#define KING_ACTIVITY_H
+#define KING_ACTIVITY_H
+
+#include "board.h"
+
+// Function to evaluate king activity
+int evaluate_king_activity(const board_t *board, int colour);
+
+#endif // KING_ACTIVITY_H
