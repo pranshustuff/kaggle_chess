881628ed6068c8faf24cebf1e8027290a17738b6
Aarush-Kanipakam <157634160+Aarush-Kanipakam@users.noreply.github.com>
2024-12-20 18:34:18 +0530

    Update uci.cpp

--- stockfish-5-src/src_c++11/uci.cpp
+++ stockfish-5-src/src_c++11/uci.cpp
@@ -196,7 +196,7 @@ void UCI::loop(int argc, char* argv[]) {
                     << dec << nouppercase << setfill(' ') << sync_endl;
 */
       else if (token == "uci")
-          sync_cout << "id name " << engine_info(true) << "\nuciok"  << sync_endl;
+          //sync_cout << "id name " << engine_info(true) << "\nuciok"  << sync_endl;
                    // << "\n"       << Options
                     
 
